2a64165c0f0866ed4196194185cfb77a3984b523
Hugo Siqueira <hugo@agenciabee.com>
2024-04-08 15:34:16 -0300

    Update Config.inc.php

--- _app/Config.inc.php
+++ _app/Config.inc.php
@@ -7,7 +7,7 @@ if ($_SERVER['HTTP_HOST'] == 'localhost'):
     define('SIS_DB_HOST', 'localhost'); //Link do banco de dados no localhost
     define('SIS_DB_USER', 'root'); //Usuário do banco de dados no localhost
     define('SIS_DB_PASS', ''); //Senha  do banco de dados no localhost
-    define('SIS_DB_DBSA', 'workcontrol'); //Nome  do banco de dados no localhost
+    define('SIS_DB_DBSA', ''); //Nome  do banco de dados no localhost
 else:
     define('SIS_DB_HOST', 'localhost'); //Link do banco de dados no servidor
     define('SIS_DB_USER', ''); //Usuário do banco de dados no servidor
